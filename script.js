2253b59b820b7fc946979b027eb042fc47b8d320
dev <devsereti@gmail.com>
2025-06-30 10:41:08 +0300

    JavaScript added.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Select DOM elements
+const form = document.getElementById('todo-form');
+const input = document.getElementById('todo-input');
+const list = document.getElementById('todo-list');
+
+// Function to add a new todo item
+form.addEventListener('submit', function(event) {
+    event.preventDefault
+}
